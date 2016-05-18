cba1e4f254b6ca1a57c472f44126d3fb07e1e4ed
alirni <aliramezani19.1@gmail.com>
2016-05-18 07:53:34 +0430

    add startTime function

--- 0-structure/script.js
+++ 0-structure/script.js
@@ -9,8 +9,23 @@
 
   $btnstart = $('.btn-start'),
 
+  endGame = function () {
+    console.log('endGame');
+
+  },
+
   startTime = function() {
     console.log('startTime');
+    while ($sec === 0) {
+      $sec --;
+      if ($sec === 0) {
+        $min --;
+        if ($min === 0 ) {
+          endGame();
+        }
+        $sec = 60;
+      }
+    }
   },
 
   start = function () {
