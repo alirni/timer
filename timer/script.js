e55d7677bb71b6bd38bddcf7f0fe39b0d152e2e7
alirni <aliramezani19.1@gmail.com>
2016-05-18 08:51:35 +0430

    add update time

--- timer/script.js
+++ timer/script.js
@@ -19,22 +19,25 @@
 
     while ($sec != 0) {
       $sec --;
+      updateTime ();
       if ($sec == 0) {
         $min --;
+        updateTime ();
         if ($min == 0 ) {
           endGame();
         }
         else {
           $sec = 60;
+          updateTime ();
         }
       }
     }
+  },
 
   updateTime = function () {
     console.log('updateTime');
 
-
-
+    
   },
 
   start = function () {
