53985d0ecc95fda841f2a181883a41f3532380f3
alirni <aliramezani19.1@gmail.com>
2016-05-18 07:39:27 +0430

    make function start time and get variable

--- 0-structure/script.js
+++ 0-structure/script.js
@@ -3,9 +3,24 @@
   'use strict';
 
   var
+  $min = $('.minute').html(),
+
+  $sec = $('.second').html(),
+
+  $btnstart = $('.btn-start'),
+
+  startTime = function() {
+    console.log('startTime');
+  },
+
+  start = function () {
+    console.log('start');
+    $btnstart.click(startTime);
+  },
 
   main = function () {
     console.log('Timer, Init ;)');
+    start ();
   }
   ;
 
